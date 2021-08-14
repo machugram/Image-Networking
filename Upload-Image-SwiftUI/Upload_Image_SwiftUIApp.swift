555c0be5a4f0068da3356c99dc306fc017c974ff
Rexford Machu <rexfordmachu@gmail.com>
2021-08-14 17:57:06 +0000

    Initial Commit

---
+++ Upload-Image-SwiftUI/Upload_Image_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Upload_Image_SwiftUIApp.swift
+//  Upload-Image-SwiftUI
+//
+//  Created by Rexford Machu on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Upload_Image_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
